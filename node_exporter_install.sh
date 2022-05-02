0258260a67eba25352dae691d95018e335c4830f
Jack Keeley <Jack@Nester.me>
2022-05-02 15:05:02 -0500

    initial commit

---
+++ node_exporter_install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#Bash script for installing node_exporter, downloads version 1.3.1, extracts it, creates a service user, and creates a systemd unit file
+
+echo "Downloading Node Exporter v1.3.1"
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+echo "Extracting Node Exporter"
+tar -xvf node_exporter-1.3.1.linux-amd64.tar.gz
+
+echo "Moving Node Exporter binary"
+
+cd node_exporter-1.3.1.linux-amd64
+sudo cp node_exporter /usr/local/bin/
+
+echo "Adding node_exporter service account"
+sudo useradd -rs /bin/false node_exporter
+
+echo "Creating systemd unit file -- node_exporter.service"
+
+sudo cat << EOF >> /etc/systemd/system/node_exporter.service
+
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "Reloading systemd"
+
+sudo systemctl daemon-reload
+
+echo "Enabling node_exporter service"
+sudo systemctl enable node_exporter
+
+echo "Restarting Node Exporter"
+sudo systemctl restart node_exporter
+
+echo "Displaying Node Exporter Status"
+sudo systemctl status node_exporter
